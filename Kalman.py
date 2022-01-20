cf57eae7c0841d5fc9daf0563105735b66cf8bdd
Nathan Doumeche <nathan.doumeche@gmail.com>
2022-01-20 15:16:25 +0100

    Filtre de Kalman

---
+++ Kalman.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def kalmanPred(xin, F, B, u, Pini, Q):
+    xpred = np.dot(F, xin) + np.dot(B,u)
+    Ppred = np.dot(np.dot(F, Pini), np.transpose(F)) + Q
+    return xpred, Ppred
+
+def KalmanUpdate(xpred, Ppred, z, H, R):
+    y = z - np.dot(H, xpred)
+    S = np.dot(np.dot(H, Ppred), np.transpose(H)) + R
+    K = np.dot(np.dot(Ppred, np.transpose(H)), np.linalg.inv(S))
+    xup = xpred + np.dot(K, y)
+    Pup = np.dot((np.identity(len(K))-np.dot(K,H)),Ppred)
+    return xup, Pup
+
+xin = np.array([0,1])
+F = np.identity(2)
+B = np.identity(2)
+u = ([1,1])
+Pini = np.identity(2)
+Q = np.identity(2)
+xpred, Ppred = kalmanPred(xin, F, B, u, Pini, Q)
+print(xpred, Ppred)
+
+z = np.array([1.1, 1.9])
+H = np.identity(2)
+R = 10*np.identity(2)
+xup, Pup = KalmanUpdate(xpred, Ppred, z, H, R)
+print(xup, Pup)
