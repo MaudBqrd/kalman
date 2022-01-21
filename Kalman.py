765c6b6b2a097b6ff9b446f95fc9acec93933bfa
Maud Biquard <biqmau@gmail.com>
2022-01-21 11:05:25 +0100

    classical kalman filter

--- Kalman.py
+++ Kalman.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 def kalmanPred(xin, F, B, u, Pini, Q):
@@ -17,23 +18,88 @@ def KalmanUpdate(xpred, Ppred, z, H, R):
 
     return xup, Pup
 
+def KalmanPred1D(x_hat, F, B, u, P_hat, Q):
+    x_hat = F*x_hat + B*u
+    P_hat = F*P_hat*F + Q
+    return x_hat, P_hat
+
+def KalmanUpdate1D(x_hat, P_hat, z, H, R):
+    y = z - H*x_hat
+    S = H*P_hat*H + R
+    K = P_hat * H / S
+    x_hat = x_hat + K*y
+    P_hat = (1 - K*H)*P_hat
+    return x_hat, P_hat
+
+
+def KalmanFilter1D(a_vehicle, v_wheel, R_tab, Q_tab):
+
+    v_hat = 0
+    P_hat = 0
+    t_final = len(a_vehicle)
+
+    v_predict_hat_tab = np.zeros(len(a_vehicle))
+    v_update_hat_tab = np.zeros(len(a_vehicle))
+    P_hat_tab = np.zeros(2*len(a_vehicle))
+
+    for t in range(t_final):
+        v_hat, P_hat = KalmanPred1D(v_hat, F=1, B=1, u=a_vehicle[t], P_hat=P_hat, Q=Q_tab[t])
+        v_predict_hat_tab[t] = v_hat
+        P_hat_tab[2*t] = P_hat
+        v_hat, P_hat = KalmanUpdate1D(v_hat, P_hat, z=v_wheel[t], H=1, R=R_tab[t])
+        v_update_hat_tab[t] = v_hat
+        P_hat_tab[2 * t + 1] = P_hat
+
+    return v_predict_hat_tab, v_update_hat_tab, P_hat_tab
+
+
+def KalmanVisualisation(v_predict_hat_tab, v_update_hat_tab, P_hat_tab, v_vehicle, v_wheel, a_vehicle, plot_all=True):
+    x1 = np.arange(len(v_vehicle))
+    x2 = np.linspace(0,len(P_hat_tab)/2, num=len(P_hat_tab))
+    plt.plot(x1, v_vehicle, label="v vehicle")
+    plt.plot(x1, v_update_hat_tab, label = "v hat (after update)")
+    plt.legend()
+
+    if plot_all:
+
+        plt.figure()
+        plt.plot(x1, v_wheel, label="v wheel")
+        plt.plot(x1, v_update_hat_tab, label="v hat (after update)")
+        plt.legend()
+
+        plt.figure()
+        plt.plot(x1, v_vehicle, label="v vehicle")
+        plt.plot(x1, v_predict_hat_tab, label='v hat prediction')
+        plt.plot(x1, v_update_hat_tab, label='v hat update')
+        plt.legend()
+
+        plt.figure()
+        plt.plot(x2, P_hat_tab, label="P hat")
+        plt.legend()
+
+
+def eval_kalman(v_update_hat_tab, v_vehicle):
+    mse = np.mean(np.square(v_update_hat_tab - v_vehicle))
+    print(f"MSE: {mse}")
+    return mse
+
+
 
 if __name__ == '__main__':
 
-    Niter = 1
-    xin = np.array([0, 1])
-    Pini = np.identity(2)
-    F = np.identity(2)
-    B = np.identity(2)
-    u = ([1, 1])
-    Q = np.identity(2)
+    path_to_traj = "/home/maud/Documents/mines/mareva/mini_projet/kalman_dataset/train/train_length_490_seq_0.npy"
 
-    for iter in range(Niter):
-        xpred, Ppred = kalmanPred(xin, F, B, u, Pini, Q)
-        print(xpred, Ppred)
+    data = np.load(path_to_traj)
+    a_vehicle = data[0]
+    v_wheel = data[1]
+    v_vehicle = data[2]
 
-        z = np.array([1.1, 1.9])
-        H = np.identity(2)
-        R = 10*np.identity(2)
-        xup, Pup = KalmanUpdate(xpred, Ppred, z, H, R)
-        print(xup, Pup)
+    # R_tab = np.array(len(a_vehicle)*[10])
+    R_tab = 2*np.abs(a_vehicle)
+    Q_tab = np.array(len(a_vehicle)*[0.25])
+
+    v_predict_hat_tab, v_update_hat_tab, P_hat_tab = KalmanFilter1D(a_vehicle, v_wheel, R_tab, Q_tab)
+    KalmanVisualisation(v_predict_hat_tab, v_update_hat_tab, P_hat_tab, v_vehicle, v_wheel, a_vehicle, plot_all=False)
+    mse = eval_kalman(v_update_hat_tab, v_vehicle)
+
+    plt.show()
--- utils/simu_to_database.py
+++ utils/simu_to_database.py
@@ -17,8 +17,15 @@ def simu_to_dataset(path_dataset_simu, path_new_dataset, std_noise):
     if not os.path.exists(join(path_new_dataset, 'train')):
         os.makedirs(join(path_new_dataset, 'train'))
 
+    if not os.path.exists(join(path_new_dataset, 'val')):
+        os.makedirs(join(path_new_dataset, 'val'))
+
     if not os.path.exists(join(path_new_dataset, 'test')):
         os.makedirs(join(path_new_dataset, 'test'))
+
+    compteur_train = 0
+    compteur_test = 0
+    computeur_val = 0
 
     for file in list_files:
 
@@ -37,18 +44,21 @@ def simu_to_dataset(path_dataset_simu, path_new_dataset, std_noise):
             to_save = np.zeros((3, len(a_vehicle)))
 
             if 'TEST' in file:
-                save_path = join(path_new_dataset, 'test', f'test_length_{len(a_vehicle)}_seq_{i}.npy')
+                save_path = join(path_new_dataset, 'test', f'test_length_{len(a_vehicle)}_seq_{compteur_test}.npy')
+                compteur_test += 1
             else:
-                save_path = join(path_new_dataset, 'train', f'train_length_{len(a_vehicle)}_seq_{i}.npy')
+                if np.random.binomial(1, 0.9, size=1) == 1:
+                    save_path = join(path_new_dataset, 'train', f'train_length_{len(a_vehicle)}_seq_{compteur_train}.npy')
+                    compteur_train += 1
+                else:
+                    save_path = join(path_new_dataset, 'val', f'val_length_{len(a_vehicle)}_seq_{computeur_val}.npy')
+                    computeur_val += 1
 
             to_save[0] = a_vehicle
             to_save[1] = v_wheel
             to_save[2] = v_vehicle
 
             np.save(save_path, to_save)
-
-
-
 
 
 
